d746a7300c9b86778fd707d478574cf651f1283d
ivan <9spotzs@gmail.com>
2025-10-14 00:20:24 -0500

    Actualizado con más sobrecargas y uso de super

--- src/main/java/modelo/Rectangulo.java
+++ src/main/java/modelo/Rectangulo.java
@@ -18,6 +18,33 @@ public class Rectangulo extends Figura {
         this.y2 = y2;
     }
     
+    public Rectangulo(double x1, double y1, double ancho, double alto, boolean usarDimensiones) {
+        super(x1, y1);
+        
+        if (usarDimensiones) {
+            if (ancho > 0) {
+                if (alto > 0) {
+                    this.x2 = x1 + ancho;
+                    this.y2 = y1 + alto;
+                } else {
+                    this.x2 = x1 + ancho;
+                    this.y2 = y1 + ancho;
+                }
+            } else {
+                if (alto > 0) {
+                    this.x2 = x1 + alto;
+                    this.y2 = y1 + alto;
+                } else {
+                    this.x2 = x1 + 10;
+                    this.y2 = y1 + 10;
+                }
+            }
+        } else {
+            this.x2 = ancho;
+            this.y2 = alto;
+        }
+    }
+    
     @Override
     public double calcularArea() {
         double base = Math.abs(x2 - x1);
@@ -36,6 +63,30 @@ public class Rectangulo extends Figura {
         
         x2 = x1 + (diferenciaX * factor);
         y2 = y1 + (diferenciaY * factor);
+    }
+    
+    public void cambiarEscala(double factorX, double factorY) {
+        double diferenciaX = x2 - x1;
+        double diferenciaY = y2 - y1;
+        
+        x2 = x1 + (diferenciaX * factorX);
+        y2 = y1 + (diferenciaY * factorY);
+    }
+    
+    public void cambiarEscala(double factor, boolean validar) {
+        if (validar) {
+            if (factor > 0) {
+                if (factor <= 10) {
+                    cambiarEscala(factor);
+                } else {
+                    cambiarEscala(10.0);
+                }
+            } else {
+                System.out.println("Factor inválido, no se aplica cambio");
+            }
+        } else {
+            cambiarEscala(factor);
+        }
     }
     
     public boolean puntoEstaDentro(double x, double y) {
@@ -68,10 +119,12 @@ public class Rectangulo extends Figura {
     
     @Override
     public String toString() {
-        return "Rectángulo:\n" +
-               "Vértice 1: (" + x1 + ", " + y1 + ")\n" +
-               "Vértice 2: (" + x2 + ", " + y2 + ")\n" +
-               "Área: " + calcularArea();
+        String infoBase = super.toString();
+        
+        return infoBase + "\n" +
+               "Punto final: (" + x2 + ", " + y2 + ")\n" +
+               "Dimensiones: " + Math.abs(x2 - x1) + " x " + Math.abs(y2 - y1) + "\n" +
+               "Área: " + calcularArea() + " unidades²";
     }
     
     public double getX2() {
