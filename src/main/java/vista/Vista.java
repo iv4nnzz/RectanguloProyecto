23a74d671e1a9a82f52c812bd71e9a47c3e26db0
ivan <9spotzs@gmail.com>
2025-10-16 23:31:19 -0500

    Metodo mostrar mensaje y mostrar error que usaremos en otras funciones

--- src/main/java/vista/Vista.java
+++ src/main/java/vista/Vista.java
@@ -3,10 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package vista;
+import javax.swing.JOptionPane;
 /**
  *
  * @author 9spot
  */
 public class Vista {
-
+    public void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje, 
+                                     "Información", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    public void mostrarError(String error) {
+        JOptionPane.showMessageDialog(null, error, 
+                                     "Error", JOptionPane.ERROR_MESSAGE);
+    }
 }
