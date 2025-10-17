df7a827c6ebdc66676d13119d9301da64317b206
ivan <9spotzs@gmail.com>
2025-10-17 00:15:19 -0500

    Añadir los atributos de la clase controlador y su respectivo constructor

--- src/main/java/controlador/Controlador.java
+++ src/main/java/controlador/Controlador.java
@@ -3,11 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
-
+import modelo.Rectangulo;
+import vista.Vista;
 /**
  *
  * @author 9spot
  */
 public class Controlador {
-
+    private Rectangulo rectangulo;
+    private Vista vista;
+    
+    public Controlador(Vista vista) {
+        this.vista = vista;
+        this.rectangulo = null;
+    }
 }
